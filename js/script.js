578dce6bbbb34718121fab87d80bf347c30a06c5
AlexTimoncini <alextimonci@gmail.com>
2023-05-17 14:44:13 +0200

    added style + linked dice game to btn

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-const userDice = Math.random() * (6 - 1) + 1;
-const botDice = Math.random() * (6 - 1) + 1;
-console.log('Your number is : ' + userDice.toFixed(0));
-console.log('Bot number is : ' + botDice.toFixed(0));
-if (userDice.toFixed(0) > botDice.toFixed(0)){
-    console.log('You won!');
-} else if (userDice.toFixed(0) < botDice.toFixed(0)){
-    console.log('You lose! :(');
-} else {
-    console.log('DRAW!')
-}
-
 const loginBtn = document.getElementById('loginBtn');
 const loggedMail = [
     'alex@boolean.com',
@@ -23,18 +11,47 @@ const loggedMail = [
     'pollo@boolean.com'
 ];
 const loginMsg = document.querySelector('pre#output')
+const playBtn = document.getElementById('playGame');
 
 loginBtn.addEventListener('click', function(){
     let userMail = document.querySelector('input#userMail');
     for (let i = 0; i < loggedMail.length; i++){
         if (userMail.value === loggedMail.at(i)){
-            loginMsg.innerHTML = 'WELCOME BACK';
+            loginMsg.innerHTML = 'WELCOME BACK!';
+
+            loginMsg.classList.add('text-success');
+            loginMsg.classList.remove('text-danger');
+
             userMail.classList.add('form-control-plaintext');
+            userMail.setAttribute("disabled", "");
+
             loginBtn.classList.add('d-none');
+
+            playBtn.removeAttribute('disabled');
         }
     }
-    if (loginMsg.innerHTML != 'WELCOME BACK'){
+    if (loginMsg.innerHTML != 'WELCOME BACK!'){
         loginMsg.innerHTML = 'WRONG MAIL, PLEASE TYPE AGAIN!';
+        loginMsg.classList.add('text-danger');
     }
 });
 
+playBtn.addEventListener('click', function(){
+    const userDice = Math.random() * (6 - 1) + 1;
+    const botDice = Math.random() * (6 - 1) + 1;
+
+    const userResult = document.getElementById('userResult');
+    const botResult = document.getElementById('botResult');
+    const finalResult = document.getElementById('finalResult');
+
+    userResult.innerHTML = 'Your number is : ' + userDice.toFixed(0);
+    botResult.innerHTML = 'Bot number is : ' + botDice.toFixed(0);
+
+    if (userDice.toFixed(0) > botDice.toFixed(0)){
+        finalResult.innerHTML = 'You won!';
+    } else if (userDice.toFixed(0) < botDice.toFixed(0)){
+        finalResult.innerHTML = 'You lose! :(';
+    } else {
+        finalResult.innerHTML = 'DRAW!'
+}
+});
