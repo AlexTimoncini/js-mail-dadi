15fa81b40f775ec209aed57ea0cf67bf77f8b9a8
AlexTimoncini <alextimonci@gmail.com>
2023-05-17 14:14:55 +0200

    added program to enter and verify user mail

--- js/script.js
+++ js/script.js
@@ -9,3 +9,32 @@ if (userDice.toFixed(0) > botDice.toFixed(0)){
 } else {
     console.log('DRAW!')
 }
+
+const loginBtn = document.getElementById('loginBtn');
+const loggedMail = [
+    'alex@boolean.com',
+    'giorgia@boolean.com',
+    'laura@boolean.com',
+    'paolo@boolean.com',
+    'fabio@boolean.com',
+    'robarta@boolean.com',
+    'napoli@boolean.com',
+    'sazio@boolean.com',
+    'pollo@boolean.com'
+];
+const loginMsg = document.querySelector('pre#output')
+
+loginBtn.addEventListener('click', function(){
+    let userMail = document.querySelector('input#userMail');
+    for (let i = 0; i < loggedMail.length; i++){
+        if (userMail.value === loggedMail.at(i)){
+            loginMsg.innerHTML = 'WELCOME BACK';
+            userMail.classList.add('form-control-plaintext');
+            loginBtn.classList.add('d-none');
+        }
+    }
+    if (loginMsg.innerHTML != 'WELCOME BACK'){
+        loginMsg.innerHTML = 'WRONG MAIL, PLEASE TYPE AGAIN!';
+    }
+});
+
